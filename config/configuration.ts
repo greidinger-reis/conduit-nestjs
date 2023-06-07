709ca0eff8ba75ba6cf9a7e5b33190834d87e10a
greidinger-reis <jp.greidinger@hotmail.com>
2023-06-07 09:01:17 -0300

    rm logs

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,5 +1,4 @@
 export const defaultConfig = () => {
-    console.log("Loading configuration", process.env)
     if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
     if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not set")
 
